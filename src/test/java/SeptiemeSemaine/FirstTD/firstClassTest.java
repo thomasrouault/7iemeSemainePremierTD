5108b7c707e743e7ace1f0d902d8d2745d713343
Thomas Rouault <throuault44@gmail.com>
2019-03-06 09:37:21 +0100

    Add junitTest to FirstTD

--- src/test/java/SeptiemeSemaine/FirstTD/firstClassTest.java
+++ src/test/java/SeptiemeSemaine/FirstTD/firstClassTest.java
@@ -1,1 +1,20 @@
 package SeptiemeSemaine.FirstTD;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class firstClassTest{
+	
+	@Test
+	public  void additionTest() {
+		int sum = Hello.addition();
+		assertEquals(80, sum);
+	}
+	
+	@Test
+	public  void soustractionTest() {
+		int sum = Hello.soustraction();
+		assertEquals(44, sum);
+	}
+}
